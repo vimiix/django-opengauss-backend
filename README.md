50a4a09800fcc468d5dc6f9e0b4c31c387d4ab97
vimiix <i@vimiix.com>
2024-07-17 23:57:30 +0800

    feat: add setup.py

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # django-opengauss-backend
+
+openGauss database dialect for django
+
+## Requirements
+
+- openGauss pysocpg2
+- django >= 1.8
+
+## Django settings
+
+You can set the name in your settings.py as:
+
+```python
+DATABASES = {
+    'default': {
+        'ENGINE': 'django_opengauss_backend',
+        'NAME': '<database name>',
+        'USER': '<database username>',
+        'PASSWORD': '<database password>',
+        'HOST': '<database host>',
+        'PORT': '5432',
+    }
+}
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='django-opengauss-backend',
+    version='0.1.0',
+    description='openGauss database dialect for django',
+    long_description=open("README.md").read(),
+    author='Vimiix',
+    author_email='i@vimiix.com',
+    license="MIT",
+    url='https://github.com/vimiix/django-opengauss-backend',
+    packages=['django-opengauss-backend'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
